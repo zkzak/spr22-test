1cce51af2973a5151c17727876fc51bc51893769
zkzak <zakbaani@gmail.com>
2022-01-31 17:40:36 -0600

    init commit

---
+++ src/main/java/com/example/Spring22Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring22/Spring22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
